4e6d68d077828b859b05b7fc16afcbdae2c803f3
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-10-15 00:08:43 +0100

    add controllers

---
+++ controllers/friends.js
@@ -0,0 +1,42 @@
+
+const friends=[
+    {id:0,name:"ahmed"},
+    {id:1,name:"taoufik1"}
+  ]
+
+
+
+const getAllFriends=(req,res)=>{
+    res.json(friends)
+}
+
+
+function getOneFriend(req,res){
+    const friendId=Number(req.params.friendId)
+    const friend=friends[friendId]
+    if (friend) {
+        res.status(200).send(friend)
+    } else {
+      res.status(404).json({message:{error:"this friend does not exist"}})
+    }
+
+}
+
+function addFriend(req,res){
+    if (!req.body.name) {
+     return res.status(400).json({error:"missing name of friend"})
+    }
+
+    const friend={
+      id:friends.length,
+      name:req.body.name
+    }
+    friends.push(friend)
+    res.status(200).json([friend,{message:{success:"friend has been created"}}])
+}
+
+module.exports={
+    getAllFriends,
+    getOneFriend,
+    addFriend
+}
--- server.js
+++ server.js
@@ -1,45 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const friends=require('./controllers/friends')
 
-
-const friends=[
-  {id:0,name:"ahmed"},
-  {id:1,name:"taoufik1"}
-]
 
 // middleware to use json format in post request
 app.use(express.json())
 
-app.post('/friends',(req,res)=>{
-    if (!req.body.name) {
-     return res.status(400).json({error:"missing name of friend"})
-    }
-
-    const friend={
-      id:friends.length,
-      name:req.body.name
-    }
-    friends.push(friend)
-    res.status(200).json([friend,{message:{success:"friend has been created"}}])
-})
+app.post('/friends',friends.addFriend)
 
 
-
-app.get('/friends',(req,res)=>{
-    res.json(friends)
-})
-
-app.get('/friends/:friendId',(req,res)=>{
-    const friendId=Number(req.params.friendId)
-    const friend=friends[friendId]
-    if (friend) {
-        res.status(200).send(friend)
-    } else {
-      res.status(404).json({message:{error:"this friend does not exist"}})
-    }
-
-})
+app.get('/friends',friends.getAllFriends)
+app.get('/friends/:friendId',friends.getOneFriend)
 
 
 
