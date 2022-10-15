79167a85c631a3e9fa83b6a500ca2577f02ff9ee
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-10-15 16:50:35 +0100

    add get one friend test

--- __test__/friends.spec.js
+++ __test__/friends.spec.js
@@ -1,5 +1,6 @@
 const request =require('supertest')
 const {app}=require('../app')
+const friends=require('../models/friends')
 
 describe('Test Get /friends',()=>{
     test('It should respond with 200 success',async()=>{
@@ -11,15 +12,46 @@ describe('Test Get /friends',()=>{
     })
 })
 
+describe('Test Get /friends/:id',()=>{
+    test('It should respond with 200 success',async()=>{
+        const response=await request(app)
+        .get('/friends/0')
+        .expect('Content-Type', /json/)
+        .expect(200)
+       
+    })
+
+    test('It should respond with 404 error',async()=>{
+        const response=await request(app)
+        .get('/friends/-1')
+        .expect('Content-Type', /json/)
+        .expect(404)
+       
+    })
+
+})
+
+
 describe('Test Post /friends',()=>{
     test('It should respond with 201 success',async()=>{
         const response=await request(app)
         .post('/friends')
         .send({
-            id:2,
+            id:friends.length,
             name:"test_1"
         })
         .expect('Content-Type', /json/)
         .expect(201)
     })
+    test('It should respond with 400 error',async()=>{
+        const response=await request(app)
+        .post('/friends')
+        .send({
+            id:friends.length,
+            // name:"test_1"
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
+    })
+
 })
