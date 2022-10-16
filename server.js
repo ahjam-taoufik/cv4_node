4e2ef50711883b42643f050a8e6dfddc802a6ac7
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-10-16 12:54:55 +0100

    add delay duration

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const port = 3000
 // middleware to use json format in post request
 // app.use(express.json())
 
+function delay(duration){
+  const startTime=Date.now()
+  while (Date.now() - startTime<duration) {
+    // event loop is blocked . . . . 
+  }
+}
+
+app.get('/timer',(req,res)=>{
+  delay(8000)
+   res.send(` tik tik tik. . . : ${process.pid}`)
+})
+
 
 
 app.listen(port, () => {
